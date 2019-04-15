abd8e82094582533690ffcb4a94f2605f68850db
VjacheslavZ <slavikov.net@mail.ru>
2019-04-15 22:33:09 +0300

    126 Displaying Cart Items on the Cart Page

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,22 +1,61 @@
 const Product = require('../models/product');
 
 exports.getAddProduct =  (req, res, next) => {
-   res.render('admin/add-product', {
+   res.render('admin/editProduct', {
       pageTitle: 'Add product',
       path: '/admin/add-product',
-      formsCSS: true,
-      activeAddProduct: true,
+      editing: false,
    })
 };
 
 exports.postAddProduct = (req, res, next) => {
    const { title, imageUrl, price, description } = req.body;
-   const product = new Product(title, imageUrl, price, description);
+   const product = new Product(null, title, imageUrl, price, description);
 
    product.save();
 
    res.redirect('/');
 };
+
+exports.postEditProduct = (req, res, next) => {
+   const prodId = req.body.productId;
+   const updatedTitle = req.body.title;
+   const updatedPrice = req.body.price;
+   const updatedImageUrl = req.body.imageUrl;
+   const updatedDesc = req.body.description;
+
+   const updatedProduct = new Product(
+      prodId,
+      updatedTitle,
+      updatedImageUrl,
+      updatedPrice,
+      updatedDesc
+   );
+
+   updatedProduct.save();
+   res.redirect('/admin/products');
+};
+
+exports.getEditProduct =  (req, res, next) => {
+   const editMode = req.query.edit;
+   if(!editMode) {
+      return res.redirect('/')
+   }
+
+   const prodId = req.params.productId;
+   Product.findById(prodId, product => {
+      if(!product) {
+         return res.redirect('/');
+      }
+      res.render('admin/editProduct', {
+         pageTitle: 'Add product',
+         path: '/admin/edit-product',
+         editing: editMode,
+         product: product,
+      })
+   });
+};
+
 
 exports.getProducts = (req, res, next) => {
    Product.fetchAll((products) => {
@@ -26,5 +65,11 @@ exports.getProducts = (req, res, next) => {
          path: '/admin/products'
       });
    });
+};
+
+exports.postDeleteProduct = (req, res, next) => {
+   const prodId = req.body.productId;
+   Product.deleteById(prodId);
+   res.redirect('/admin/products');
 };
 

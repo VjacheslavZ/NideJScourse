203132ea6e4f71aa5c99f7695d1d316aa405bb09
VjacheslavZ <slavikov.net@mail.ru>
2019-04-22 23:38:45 +0300

    166 Adding an Order Model

--- app.js
+++ app.js
@@ -5,10 +5,15 @@ const bodyParser = require('body-parser');
 
 const errorController = require('./controllers/error');
 const sequelize = require('./util/database');
+
 const Product = require('./models/product');
 const User = require('./models/user');
+
 const Cart = require('./models/cart');
 const CartItem = require('./models/cart-item');
+
+const Order = require('./models/order');
+const OrderItem = require('./models/order-item');
 
 const app = express();
 
@@ -37,11 +42,13 @@ app.use(errorController.get404);
 
 Product.belongsTo(User, { constraints: true, onDelete: 'CASCADE'});
 User.hasMany(Product);
-
 User.hasOne(Cart);
 Cart.belongsTo(User);
 Cart.belongsToMany(Product, { through: CartItem });
 Product.belongsToMany(Cart, { through: CartItem });
+Order.belongsTo(User);
+User.hasMany(Order);
+Order.belongsToMany(Product, { through: OrderItem });
 
 sequelize
    // .sync({ force: true })

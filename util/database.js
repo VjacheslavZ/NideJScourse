60c4195b58e0c5d69415196fcbc06c4cac4661de
VjacheslavZ <slavikov.net@mail.ru>
2019-04-26 22:32:19 +0300

    177 Finishing the Database Connection

--- util/database.js
+++ util/database.js
@@ -1,15 +1,29 @@
 const mongodb = require('mongodb');
 const MongoClient = mongodb.MongoClient;
 
+let _db;
+
 const mongoConnect = (callback) => {
    MongoClient.connect(
       'mongodb+srv://Vjacheslav:zxcv1234@cluster0-e2moq.mongodb.net/test?retryWrites=true'
    )
       .then(client => {
          console.log('MongoDB connected');
-         callback(client);
+         _db = client.db();
+         callback();
       })
-      .catch(err => console.log(err));
+      .catch(err => {
+         console.log(err);
+         throw err;
+      });
 };
 
-module.exports = mongoConnect;
+const getDb = () => {
+   if (_db) {
+      return _db;
+   }
+   throw 'No database found';
+};
+
+exports.mongoConnect = mongoConnect;
+exports.getDb = getDb;

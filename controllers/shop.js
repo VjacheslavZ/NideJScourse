a6b98198f3cf656c18f6aa255dde9ce7a08b141c
VjacheslavZ <slavikov.net@mail.ru>
2019-04-27 21:46:37 +0300

    195 Fixing a Bug

--- controllers/shop.js
+++ controllers/shop.js
@@ -72,10 +72,11 @@ exports.postCart = (req, res, next) => {
    Product
       .findById(prodId)
       .then(product => {
-         return req.user.addToCart(product)
+         return req.user.addToCart(product);
       })
       .then(result => {
          console.log(result);
+         res.redirect('/cart')
       })
       .catch(err => console.log(err))
    // let fetchedCart;

a50649780d065e02212fad3b66c5fec62c8af0db
VjacheslavZ <slavikov.net@mail.ru>
2019-04-27 17:16:26 +0300

    187 Deleting Products

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,7 +1,4 @@
-const mongodb = require('mongodb');
 const Product = require('../models/product');
-
-const ObjectId = mongodb.ObjectId;
 
 exports.getAddProduct =  (req, res, next) => {
    res.render('admin/editProduct', {
@@ -35,7 +32,7 @@ exports.postEditProduct = (req, res, next) => {
          updatedPrice,
          updatedDesc,
          updatedImageUrl,
-         new ObjectId(prodId)
+         prodId
       );
       product
          .save()
@@ -77,16 +74,12 @@ exports.getProducts = (req, res, next) => {
       .catch(err => console.log(err))
 };
 
-// exports.postDeleteProduct = (req, res, next) => {
-//    const prodId = req.body.productId;
-//    Product
-//       .findByPk(prodId)
-//       .then(product => {
-//          return product.destroy()
-//       })
-//       .then(result => {
-//          console.log('product removed');
-//          res.redirect('/admin/products');
-//       })
-//       .catch(err => console.log(err));
-// };
+exports.postDeleteProduct = (req, res, next) => {
+   const prodId = req.body.productId;
+   Product.deleteById(prodId)
+      .then(() => {
+         console.log('product removed');
+         res.redirect('/admin/products');
+      })
+      .catch(err => console.log(err));
+};
--- models/product.js
+++ models/product.js
@@ -7,7 +7,7 @@ class Product {
       this.price = price;
       this.description = description;
       this.imageUrl = imageUrl;
-      this._id = id;
+      this._id = new mongodb.ObjectId(id);
    }
 
    save() {
@@ -18,10 +18,7 @@ class Product {
          //Update prod
          dbOp = db
             .collection('products')
-            .updateOne(
-               { _id: new mongodb.ObjectId(this._id) },
-               { $set: this }
-            );
+            .updateOne( { _id: this._id }, { $set: this } );
       } else {
          dbOp = db.collection('products').insertOne(this);
       }
@@ -56,6 +53,17 @@ class Product {
             return product;
          })
          .catch(err => console.log(err))
+   }
+
+   static deleteById(prodId) {
+      const db = getDb();
+      return db
+         .collection('products')
+         .deleteOne({ _id: new mongodb.ObjectId(prodId) })
+         .then(() => {
+            console.log('Deleted')
+         })
+         .catch(err => console.log(err));
    }
 }
 

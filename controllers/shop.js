c72ff999fda7f7c26dfd4a4df665137364e22f29
VjacheslavZ <slavikov.net@mail.ru>
2019-05-12 13:02:33 +0300

    327 Generating .pdf Files with Order Data

--- controllers/shop.js
+++ controllers/shop.js
@@ -182,7 +182,23 @@ exports.getInvoice = (req, res, next) => {
             'inline; filename="' + invoiceName + '"');
          pdfDoc.pipe(fs.createWriteStream(invoicePath));
          pdfDoc.pipe(res);
-         pdfDoc.text('hello world');
+         pdfDoc.fontSize(26).text('Invoice', {
+            underline: true,
+         });
+         pdfDoc.text('-----------------------');
+         let totalPrice = 0;
+         order.products.forEach(prod => {
+            totalPrice += prod.quantity * prod.product.price;
+
+            pdfDoc
+               .fontSize(14)
+               .text(
+                  prod.product.title + ' - ' + prod.quantity + ' x ' + '$' + prod.product.price
+               );
+         });
+         pdfDoc.text('-----------------------');
+         pdfDoc.fontSize(20).text('Total price: $ ' + totalPrice);
+
          pdfDoc.end();
       })
       .catch(err => next(err));
